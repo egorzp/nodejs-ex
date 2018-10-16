e8fc2349daf81e804da7953d7ce67e41245ffd05
Egor Romanenko <egorromanenko@Egors-MacBook-Pro.local>
2018-10-16 10:28:38 +0300

    add message

--- server.js
+++ server.js
@@ -1,45 +1,49 @@
 //  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    const webpush = require('web-push');
-    var cors = require('cors');
-    const vapidKeys = {
-      publicKey : 'BHLDLVj8bB0NaoqR_3_OYLZqBXdZjNgDGSwl4BsPDqmJdEr5Ycus8aXgAVNX2LMNMLlIvFbNmus5031TFJAm0hg',
-      privateKey : 'zKBAezmTXago9S2eBtKT_66_hWiUXELm3wsBZoqoSaQ'
-  }
-const bodyParser = require('body-parser');
-Object.assign=require('object-assign')
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const webpush = require("web-push");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+Object.assign = require("object-assign");
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'));
+const vapidKeys = {
+  publicKey:
+    "BHLDLVj8bB0NaoqR_3_OYLZqBXdZjNgDGSwl4BsPDqmJdEr5Ycus8aXgAVNX2LMNMLlIvFbNmus5031TFJAm0hg",
+  privateKey: "zKBAezmTXago9S2eBtKT_66_hWiUXELm3wsBZoqoSaQ"
+};
+const subscriptions = [];
+
+
+app.engine("html", require("ejs").renderFile);
+app.use(morgan("combined"));
 app.use(bodyParser.json());
-
 app.use(cors());
+
 webpush.setVapidDetails(
-  'mailto:eromanenko@s-pro.io',
+  "mailto:eromanenko@s-pro.io",
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
 
-const subscriptions = [];
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
+  mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+  mongoURLLabel = "";
 
 if (mongoURL == null) {
   var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
   // If using plane old env vars via service discovery
   if (process.env.DATABASE_SERVICE_NAME) {
     var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
-    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
-    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
-    mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
-    mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
-    mongoUser = process.env[mongoServiceName + '_USER'];
+    mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
+    mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
+    mongoDatabase = process.env[mongoServiceName + "_DATABASE"];
+    mongoPassword = process.env[mongoServiceName + "_PASSWORD"];
+    mongoUser = process.env[mongoServiceName + "_USER"];
 
-  // If using env vars from secret from service binding  
+    // If using env vars from secret from service binding
   } else if (process.env.database_name) {
     mongoDatabase = process.env.database_name;
     mongoPassword = process.env.password;
@@ -55,114 +59,119 @@ if (mongoURL == null) {
   }
 
   if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
+    mongoURLLabel = mongoURL = "mongodb://";
     if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
+      mongoURL += mongoUser + ":" + mongoPassword + "@";
     }
     // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+    mongoURLLabel += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
+    mongoURL += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
   }
 }
 var db = null,
-    dbDetails = new Object();
+  dbDetails = new Object();
 
 var initDb = function(callback) {
   if (mongoURL == null) return;
 
-  var mongodb = require('mongodb');
+  var mongodb = require("mongodb");
   if (mongodb == null) return;
 
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
+  mongodb.connect(
+    mongoURL,
+    function(err, conn) {
+      if (err) {
+        callback(err);
+        return;
+      }
+
+      db = conn;
+      dbDetails.databaseName = db.databaseName;
+      dbDetails.url = mongoURLLabel;
+      dbDetails.type = "MongoDB";
+
+      console.log("Connected to MongoDB at: %s", mongoURL);
     }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
+  );
 };
 
-app.get('/', function (req, res) {
+app.get("/", function(req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
-    initDb(function(err){});
+    initDb(function(err) {});
   }
   if (db) {
-    var col = db.collection('counts');
+    var col = db.collection("counts");
     // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
+    col.insert({ ip: req.ip, date: Date.now() });
+    col.count(function(err, count) {
       if (err) {
-        console.log('Error running count. Message:\n'+err);
+        console.log("Error running count. Message:\n" + err);
       }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+      res.render("index.html", { pageCountMessage: count, dbInfo: dbDetails });
     });
   } else {
-    res.render('index.html', { pageCountMessage : null});
+    res.render("index.html", { pageCountMessage: null });
   }
 });
 
-app.post('/subscribe', (req, res) => {
-  console.log('DEBUG:: req.body', req.body);
-  
-  subscriptions.push(req.body)
-  res.status(200).json({message: "Subscription added successfully."});
+// PUSH SECTION
+app.post("/subscribe", (req, res) => {
+  subscriptions.push(req.body);
+  res.status(200).json({ message: "Subscription added successfully." });
 });
 
-app.get('/send',
-  (req, res) => {
-      const notificationPayload = {
-          notification: {
-            title: 'New Notification',
-            body: 'This is the body of the notification',
-            icon: 'assets/icons/icon.png'
-          }
-      }
-      console.log('DEBUG:: subscriptions', subscriptions);
-      
-      Promise.all(subscriptions.map(sub => webpush.sendNotification(
-          sub, JSON.stringify(notificationPayload) )))
-          .then(() => res.status(200).json({message: 'Newsletter sent successfully.'}))
-          .catch(err => {
-              console.error("Error sending notification, reason: ", err);
-              res.sendStatus(500);
-          });
-  }
-);
+app.get("/send", (req, res) => {
+  const message = req.query.message || 'test message';
+  const notificationPayload = {
+    notification: {
+      title: "New Notification",
+      body: `Test PushPWA: ${message}`,
+      icon: './assets/icons/icon.png'
+    }
+  };
 
-app.get('/pagecount', function (req, res) {
+  Promise.all(
+    subscriptions.map(sub =>
+      webpush.sendNotification(sub, JSON.stringify(notificationPayload))
+    )
+  )
+    .then(() =>
+      res.status(200).json({ message: "Push sent successfully." })
+    )
+    .catch(err => {
+      console.error("Error sending notification, reason: ", err);
+      res.sendStatus(500);
+    });
+});
+
+app.get("/pagecount", function(req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
-    initDb(function(err){});
+    initDb(function(err) {});
   }
   if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
+    db.collection("counts").count(function(err, count) {
+      res.send("{ pageCount: " + count + "}");
     });
   } else {
-    res.send('{ pageCount: -1 }');
+    res.send("{ pageCount: -1 }");
   }
 });
 
 // error handling
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
   console.error(err.stack);
-  res.status(500).send('Something bad happened!');
+  res.status(500).send("Something bad happened!");
 });
 
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
+initDb(function(err) {
+  console.log("Error connecting to Mongo. Message:\n" + err);
 });
 
 app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+console.log("Server running on http://%s:%s", ip, port);
 
-module.exports = app ;
+module.exports = app;
